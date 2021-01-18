ff936463c4bbd38325813e9b3bc8ccf4cddaf865
Efkat <hugo.ben80000@gmail.com>
2021-01-18 22:18:42 +0100

    Add exercice 3 + refactor switch

--- Program.cs
+++ Program.cs
@@ -19,37 +19,42 @@ namespace TP2.Cons
             v_uint = (uint)v_ulong;
 
             Console.WriteLine("Saisissez l'exercice souhaité : ");
-            int exerciceNumber = Console.Read();
-            switch (exerciceNumber)
+            
+            int number = Console.Read() - 48;
+
+            switch (number)
             {
                 case 1:
-                    goto Exercice1;
+                    byte[] numberArray = { 7, 8, 9 };
+                    byte[] numberArray2 = { 0, 1, 2, 3, 4 };
+                    for (byte index = 0; index < 10; index++)
+                    {
+                        if (numberArray.Contains(index) || numberArray2.Contains(index))
+                        {
+                            Console.WriteLine("Numéro {0}", index);
+                        }
+                        else continue;
+                    }
+                    break;
                 case 2:
-                    goto Exercice2;
+                    string userInput;
+                    do
+                    {
+                        Console.WriteLine("Saisissez du texte : ");
+                        userInput = Console.ReadLine();
+                    } while (userInput.StartsWith("a"));
+                    break;
+                case 3:
+                    string[] days = { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+                    foreach(string day in days)
+                    {
+                        Console.WriteLine(day);
+                    }
+                    break;
                 default:
                     Console.WriteLine("Exercice non réalisé !");
                     break;
             }
-
-            Exercice1:
-                byte[] numberArray = {7, 8, 9};
-                byte[] numberArray2 = { 0,1,2,3,4};
-                for (byte index = 0; index < 10; index++)
-                {
-                    if (numberArray.Contains(index) || numberArray2.Contains(index))
-                    {
-                        Console.WriteLine("Numéro {0}", index);
-                    }
-                    else continue;
-                }
-
-            Exercice2:
-                string userInput;
-                do
-                {
-                    Console.WriteLine("Saisissez du texte : ");
-                    userInput = Console.ReadLine();
-                } while (userInput.StartsWith("a"));
 
             Console.ReadLine();
         }
