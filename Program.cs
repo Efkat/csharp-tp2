02af2962c20826490913091f4331f388fe8d8f58
Efkat <hugo.ben80000@gmail.com>
2021-01-18 23:02:41 +0100

    Add exercice 4

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ namespace TP2.Cons
             uint v_uint = 4;
             ulong v_ulong = 1400;
             v_uint = (uint)v_ulong;
+            int number = 0;
 
             Console.WriteLine("Saisissez l'exercice souhaité : ");
-            
-            int number = Console.Read() - 48;
+            number = Convert.ToInt32(Console.ReadLine());
 
             switch (number)
             {
@@ -51,12 +51,50 @@ namespace TP2.Cons
                         Console.WriteLine(day);
                     }
                     break;
+                case 4:
+                    goto exercice4;
                 default:
                     Console.WriteLine("Exercice non réalisé !");
                     break;
             }
 
-            Console.ReadLine();
+        exercice4:
+            int numberInput = 0;
+            int sum = 0;
+
+            do
+            {
+                try
+                {
+                    Console.Write("Taper un chiffre : ");
+                    numberInput = Convert.ToInt32(Console.ReadLine());
+                    sum += numberInput;
+                    if (numberInput < 50)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Le chiffre saisie doit être minimum égale à 50 !");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Veuillez saisir un chiffre valide !");
+                }
+                finally
+                {
+                    if(sum >= 50)
+                    {
+                        Console.WriteLine("La somme a atteint 50 minimum, bravo");
+                    }
+                    Console.WriteLine("La somme totale est de {0}", sum);
+                }
+            } while (sum < 50);
+
+            
+
+            
         }
     }
 }
