50e074b85799a78bab39f4230d933faf5e054284
Efkat <hugo.ben80000@gmail.com>
2021-01-25 22:41:10 +0100

    Create Windmill.cs + Tools.cs files

---
+++ Windmill.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2.Cons
+{
+    class Windmill : IOperation
+    {
+        public bool isOn { get; set; }
+
+        public string GetState()
+        {
+            if (isOn)
+            {
+                return "Allumé !";
+            }
+            else
+            {
+                return "Eteinte !";
+            }
+        }
+
+        public void TurnOff()
+        {
+            if (isOn)
+            {
+                isOn = false;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void TurnOn()
+        {
+            if (!isOn)
+            {
+                isOn = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
